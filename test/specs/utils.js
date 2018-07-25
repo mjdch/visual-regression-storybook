c89ee4fdb01450e956c732665c869896e95ceca3
mjdch <maciejo@poczta.onet.eu>
2018-07-26 00:33:14 +0200

    .gitignore fix & url logging

--- test/specs/utils.js
+++ test/specs/utils.js
@@ -1,12 +1,12 @@
 const assert = require('assert');
 
 const navigateToStory = (kind, story) =>{
+    console.log(`Navigating to: ${kind} / ${story}`)
     browser.url(browser.url(`/iframe.html?selectedKind=${kind}&selectedStory=${story}`))
 }
 
 const vrCompare = () =>{
     let report = browser.checkElement('#root');
-    console.log(report);
     report.forEach((viewport)=>{
         assert.equal(viewport.isWithinMisMatchTolerance, true);
     })
